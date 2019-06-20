37d1a0d7f904c784f44c8a730791093617a26f8f
王建冰 <18047715@suning.com>
2019-06-20 10:42:33 +0800

    Revert "提交不推送测试"
    This reverts commit af0bd9cca1c7c0460dacb49fc465da3388fffe33.

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,Image} from 'react-native';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -24,7 +24,6 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
-        <Image source={{uri:"http://qn.bingying.online/50863439.jpeg"}} style={{height:400,width:400}}/>
       </View>
     );
   }
